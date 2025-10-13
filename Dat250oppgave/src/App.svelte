85f74b19a25fcccc7be46fd6fc32b6c49bacacfd
DaanVanKessel <flagg@MSI.localdomain>
2025-10-13 19:22:27 +0200

    done task 6

---
+++ Dat250oppgave/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+    /*import './PollController.java';*/
+    let result = $state(fetch("/User").then((response) => {
+            return response.json();
+        }
+    ))
+</script>
+
+<main>
+  <div>
+    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+
+    </a>
+  </div>
+    <button>
+
+    </button>
+  <h1>Vite {result} + Svelte</h1>
+
+  <div class="card">
+  </div>
+
+
+
+</main>
